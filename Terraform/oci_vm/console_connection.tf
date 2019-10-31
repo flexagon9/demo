019e7835ca78ac16d6e8c54849cdcee7c564a8e0
Muthu Sankaralingam <muthu.sankaralingam@flexagon.com>
2019-10-31 17:06:53 +0530

    remove var

--- Terraform/oci_vm/console_connection.tf
+++ Terraform/oci_vm/console_connection.tf
@@ -102,11 +102,3 @@ data "oci_core_vnic_attachments" "instance_vnics" {
 data "oci_core_vnic" "instance_vnic" {
   vnic_id = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
 }
-
-variable "connect_with_ssh" {
-  value = "${oci_core_instance_console_connection.test_instance_console_connection.connection_string}"
-}
-
-variable "connect_with_vnc" {
-  value = "${oci_core_instance_console_connection.test_instance_console_connection.vnc_connection_string}"
-}
