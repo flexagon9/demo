96ab960bdf02c1d72c526e94df8105ab8b4a424b
Muthu Sankaralingam <muthu.sankaralingam@flexagon.com>
2019-10-31 14:18:13 +0530

    add compartment id

--- Terraform/oci_autonomousdb/provider.tf
+++ Terraform/oci_autonomousdb/provider.tf
@@ -1,4 +1,4 @@
-variable "private_key_path" {default="/home/oracle/.ssh/oci_api_key.pem"}
+variable "compartment_ocid" {default="ocid1.compartment.oc1..aaaaaaaaose23kfto7ito35t7mkowbjxgeqxshssjk4gejgfydqygbgfacja"}
 variable "region" {default="us-ashburn-1"}
 variable "autonomous_database_cpu_core_count" {default=1}
 variable "autonomous_database_data_storage_size_in_tbs" {default=1}
@@ -6,6 +6,5 @@ variable "autonomous_database_db_name" {default="flexdb"}
 
 provider "oci" {
   version              = ">= 3.14"
-  private_key_path     = "${var.private_key_path}"
   region               = "${var.region}"
 }
