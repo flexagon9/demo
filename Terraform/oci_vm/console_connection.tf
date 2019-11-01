8d4463277dff38299a946eb8c70c5cd32ee83c3f
joel-wenzel <50124194+joel-wenzel@users.noreply.github.com>
2019-11-01 16:46:10 -0500

    Update console_connection.tf

--- Terraform/oci_vm/console_connection.tf
+++ Terraform/oci_vm/console_connection.tf
@@ -68,7 +68,7 @@ resource "oci_core_instance" "test_instance" {
 
   source_details {
     source_type = "image"
-    source_id   = "ocid1.image.oc1.iad.aaaaaaaas4rje5udqcbicsqkmnssbs4bqveylq3jsrqsb4vnbzwlijqxjqgq"
+    source_id   = "ocid1.image.oc1.iad.aaaaaaaahqfv2inssrwfu6arffox3zubxm4swql4hht6t5fk4m5ilfzomgda"
   }
 
   metadata = {
