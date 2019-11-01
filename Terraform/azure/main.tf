c56b1450fa0867cbf2de88f091c28f17d6144d61
wenzje07 <joel.wenzel@flexagon.com>
2019-11-01 12:14:05 -0500

    added 9090 port

--- Terraform/azure/main.tf
+++ Terraform/azure/main.tf
@@ -54,7 +54,10 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_range     = "22"
+        destination_port_ranges    = [
+			"22",
+			"9090"
+		  ]
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
