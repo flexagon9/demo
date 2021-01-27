28395b8d6727c59e826aa7130b88e5d0b8e4a874
flexagon9 <flexagon9@gmail.com>
2021-01-26 23:39:14 -0600

    Update provider.tf

--- Terraform/oci_autonomousdb/provider.tf
+++ Terraform/oci_autonomousdb/provider.tf
@@ -2,7 +2,7 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
-variable "compartment_ocid" {default="ocid1.compartment.oc1..aaaaaaaaose23kfto7ito35t7mkowbjxgeqxshssjk4gejgfydqygbgfacja"}
+variable "compartment_ocid" {default="ocid1.compartment.oc1..aaaaaaaanqjkbw76pcrpg24tcoroxgdi2ivtwcidkce4hjv2eksg2rgpo6da"}
 variable "region" {default="us-ashburn-1"}
 variable "autonomous_database_cpu_core_count" {default=1}
 variable "autonomous_database_data_storage_size_in_tbs" {default=1}
