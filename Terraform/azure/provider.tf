26dda3e673798158749bff554a39d0df962437a3
Joel Wenzel <50124194+joel-wenzel@users.noreply.github.com>
2021-10-26 08:34:27 -0500

    Updated to variables

--- Terraform/azure/provider.tf
+++ Terraform/azure/provider.tf
@@ -1,8 +1,21 @@
 # Configure the Microsoft Azure Provider
+variable "subscriptionId" {
+  type = string
+}
+variable "clientId" {
+  type = string
+}
+variable "clientSecret" {
+  type = string
+}
+variable "tenantId" {
+  type = string
+}
+
 provider "azurerm" {
-     version = "=1.36.0"
-    subscription_id = "df4930ce-dd93-43fe-b868-9f8fae4a15b3"
-    client_id       = "ba741718-5027-4818-ba2b-6e5406254463"
-    client_secret   = "qk6/oFG:GwZxBrseCMb=Ao[G415cqg=u"
-    tenant_id       = "62968c10-0ab4-4eb8-9ba0-df1240a626eb"	 
+    version = "=1.36.0"
+    subscription_id = var.subscriptionId
+    client_id       = var.clientId
+    client_secret   = var.clientSecret
+    tenant_id       = var.tenantId	 
 }
