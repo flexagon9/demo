a4b40848b4aeb78fd65dce455602f8eecacb77af
gowthambolli <krishnagowtham.bolli@flexagon.com>
2022-01-19 15:41:48 +0530

    changes made in RunTest

--- SalesForce-TrailBlazerApp/Test/RunTest.java
+++ SalesForce-TrailBlazerApp/Test/RunTest.java
@@ -1,29 +1,42 @@
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+
+
 public class RunTest
-   extends AbstractTest
+extends AbstractTest
 {
- public RunTest()
- {
-   super();
- }
- 
+static WebDriver webDriver;
+static Actions action;
+static WebDriverWait wait;
+static String server;
+
+
+public RunTest()
+{
+super();
+}
+
 @Test
-public void login() {
- System.setProperty("webdriver.chrome.driver", "path of driver");
- WebDriver driver=new ChromeDriver();
- driver.manage().window().maximize();
- driver.get("https://www.facebook.com/");
- WebElement username=driver.findElement(By.id("email"));
- WebElement password=driver.findElement(By.id("pass"));
- WebElement login=driver.findElement(By.name("login"));
- username.sendKeys("abc@gmail.com");
- password.sendKeys("your_password");
- login.click();Thread.sleep(2000);
- String actualUrl="https://www.facebook.com/";
- String expectedUrl= driver.getCurrentUrl();
- Assert.assertEquals(expectedUrl,actualUrl);
- }
+public void login()
+{
+server = "http://129.152.142.52:7001";
+System.out.println("Testing on server " + server);
+java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.SEVERE);
+webDriver = new HtmlUnitDriver(true);
+webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+action = new Actions(webDriver);
+wait = new WebDriverWait(webDriver, 10);
+driver.get("https://www.facebook.com/");
+WebElement username=driver.findElement(By.id("email"));
+WebElement password=driver.findElement(By.id("pass"));
+WebElement login=driver.findElement(By.name("login"));
+username.sendKeys("abc@gmail.com");
+password.sendKeys("your_password");
+login.click();Thread.sleep(2000);
+String actualUrl="https://www.facebook.com/";
+String expectedUrl= driver.getCurrentUrl();
+Assert.assertEquals(expectedUrl,actualUrl);
+webDriver.close();
+}
 }
