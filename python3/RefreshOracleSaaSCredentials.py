5d50afbab7ef16978524bcc8f153da74b5e04981
flexagon9 <flexagon9@gmail.com>
2022-08-15 16:51:33 -0500

    Update RefreshOracleSaaSCredentials.py

--- python3/RefreshOracleSaaSCredentials.py
+++ python3/RefreshOracleSaaSCredentials.py
@@ -155,7 +155,8 @@ def update_saas_instance_passwords(level=0):
     driver.close()
     
     # Write the password as a text file
-    reports_dir = os.chdir('../../reports')
+    os.chdir('../../reports')
+    reports_dir = os.path.abspath(os.curdir)
     with open(f"{reports_dir}/password.txt", 'wb') as fh:
         fh.write(f"DEV_9 Password={dev_9_pass}\n")
         fh.write(f"DEV_11 Password={dev_11_pass}\n")
